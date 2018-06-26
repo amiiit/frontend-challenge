c4a8a7f29bbb1a1b8e81eaf1f2f6040eee61d166
Amit Jakubowicz <a.jakubowicz@travelaudience.com>
2018-06-26 16:57:47 +0200

    Implement download as part of the script

--- data/download.js
+++ data/download.js
@@ -1,14 +1,15 @@
 var fs = require('fs');
 var mkdirp = require('mkdirp');
 var child_process = require('child_process');
-
+const wget = require('wget-improved');
 var axios = require('axios');
+
 axios.get('https://freemusicarchive.org/featured.json?genre_handle=Blues')
   .then(function (response) {
     let tracksToDownload = 5;
-    console.log('Please execute the following to download the songs:\n');
 
     const tracksToSave = [];
+    const toDownload = [];
 
     response.data.aTracks.forEach(function (track) {
       if (!tracksToDownload) {
@@ -20,10 +21,16 @@ axios.get('https://freemusicarchive.org/featured.json?genre_handle=Blues')
       const trackName = trackPathParts.splice(trackPathParts.length - 1, 1)[0];
       const trackPath = __dirname + '/' + trackPathParts.join('/');
 
-      if (!fs.existsSync(trackPath)){
+      if (!fs.existsSync(trackPath)) {
         mkdirp(trackPath);
       }
-      console.log('wget ' + track.track_file_url + ' -O ' + trackPath + '/' + trackName + (tracksToDownload ? ' &&' : '\n'))
+
+      const dest = trackPath + '/' + trackName;
+
+      toDownload.push({
+        src: track.track_file_url,
+        output: dest,
+      });
 
       // remove references to freemusicharchive
       delete track.track_file_url;
@@ -34,13 +41,32 @@ axios.get('https://freemusicarchive.org/featured.json?genre_handle=Blues')
       tracksToSave.push(track)
     });
 
-    fs.writeFile(__dirname + '/tracks.json', JSON.stringify(tracksToSave, null, '\t'), function(err) {
-      if(err) {
+    fs.writeFile(__dirname + '/tracks.json', JSON.stringify(tracksToSave, null, '\t'), function (err) {
+      if (err) {
         return console.log(err);
       }
 
     });
-  });
+    return toDownload
+  }).then(wgetConfigs => {
+    return Promise.all(wgetConfigs.map(wgetConfig => {
+      return new Promise((resolve, reject) => {
+        const download = wget.download(wgetConfig.src, wgetConfig.output);
+        download.on('error', function(err) {
+          console.log(err);
+          reject(err);
+        });
+        download.on('start', function(fileSize) {
+          console.log('Started downloading', wgetConfig.output, fileSize);
+        });
+        download.on('end', function(output) {
+          console.info('Finished downloading', wgetConfig.output);
+          resolve();
+        });
+
+      })
+    }))
+});
 
 // const songs = require('./songs.json');
 //
